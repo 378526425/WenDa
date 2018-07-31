3252b3611d99a46048f969bdd5d44184f5a6da1f
37852 <378526425@qq.com>
2018-07-31 15:46:17 +0800

    hello 2

--- src/com/newcoder/Main.java
+++ src/com/newcoder/Main.java
@@ -3,7 +3,9 @@ package com.newcoder;
 public class Main {
     public static  void  main(String [] args)
     {
+
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.print("nihao ");
     }
 }
